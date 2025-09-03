0d7316fd89a9b14df95ea3ff389460218c8a8e9a
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-03 10:26:32 -0300

    Feat: Atividade 03

---
+++ Atividade-03/Atividade-03/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int valor;
+Console.WriteLine("Digite um valor: ");
+valor = int.Parse(Console.ReadLine());
+Console.WriteLine($"O número antecessor é: {valor -  1}");
+
