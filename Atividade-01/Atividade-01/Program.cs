9081f609fa8a4874bb896763db1f52d53be7ee34
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-03 10:20:30 -0300

    Feat: Atividade 01

--- Atividade-01/Atividade-01/Program.cs
+++ Atividade-01/Atividade-01/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Maria Eduarda!"); 
+int num1;
+int num2;
+
+Console.WriteLine("Digite um número: ")
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite outro número: ")
+num2 = int.Parse(Console.ReadLine());
+int soma = num1 + num2;
+Console.WriteLine($"A soma entre {num1} e {num2} é de {soma}");
+
