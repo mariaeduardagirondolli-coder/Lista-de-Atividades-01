700116465d2b83f290e5dc427c785dc25eb47db2
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-03 10:40:12 -0300

    Feat: Atividade 03

--- Atividade-03/Atividade-03/Program.cs
+++ Atividade-03/Atividade-03/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int valor;
-Console.WriteLine("Digite um valor: ");
-valor = int.Parse(Console.ReadLine());
-Console.WriteLine($"O número antecessor é: {valor -  1}");
+int B;
+int A;
+Console.WriteLine("Escreva as dimensões do retângulo: ");
+B = int.Parse(Console.ReadLine());
+A = int.Parse(Console.ReadLine());
+Console.WriteLine($"A área do retângulo é: {A * B}");
 
