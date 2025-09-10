99d2f47be0792fed2f63e8b4725f327d8936f10f
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-10 11:03:33 -0300

    Feat: Atividade - Extra 5

---
+++ Atividade - Extra 5/Atividade - Extra 5/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o salário do funcionário: R$ ");
+double salario = Convert.ToDouble(Console.ReadLine());
+double aumento = salario * 0.15;
+double novoSalario = salario + aumento;
+Console.WriteLine("O novo salário do funcionário com 15% de aumento é: R$ " + novoSalario.ToString("F2"));
