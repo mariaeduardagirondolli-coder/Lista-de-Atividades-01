33a425c2627f051f68d00ba45699fa2d3bf62d7e
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-10 08:45:36 -0300

    Feat: Atividade 10

---
+++ Atividade-10/Atividade-10/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+double nota1, nota2, media;
+
+Console.WriteLine("Por favor, insira a nota da 1ª avaliação: ");
+nota1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Por favor, insira a nota da 2ª avaliação: ");
+nota2 = Convert.ToDouble(Console.ReadLine());
+media = (nota1 + nota2) / 2;
+Console.WriteLine($"A média aritmética das avaliações é: {media}");
+
