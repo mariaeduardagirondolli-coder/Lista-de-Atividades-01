ad50e4a7004b037c922027dd71459e03c30bc89a
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-10 10:13:48 -0300

    Feat: Atividade - Extra 1

---
+++ Atividade - Extra 1/Atividade - Extra 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+const double cotacaoDolar = 3.45;
+Console.Write("Digite quanto dinheiro você tem na carteira (R$): ");
+string input = Console.ReadLine();
+if (double.TryParse(input, out double reais))
+{
+    double dolares = reais / cotacaoDolar;
+    Console.WriteLine($"Com R${reais:F2}, você pode comprar US${dolares:F2}.");
+}
+else
+{
+    Console.WriteLine("Valor inválido. Por favor, digite um número.");
+}
