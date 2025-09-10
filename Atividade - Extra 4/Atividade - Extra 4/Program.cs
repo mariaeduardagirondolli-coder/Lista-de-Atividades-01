f7842def3e044ff58151c5c8270664c14094c064
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-10 10:32:05 -0300

    Feat: Atividade - Extra 4

---
+++ Atividade - Extra 4/Atividade - Extra 4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o preço do produto (R$): ");
+string inputPreco = Console.ReadLine();
+if (double.TryParse(inputPreco, out double preco))
+{
+    double precoPromocional = preco * 0.95;
+    Console.WriteLine($"O preço promocional com 5% de desconto é: R${precoPromocional:F2}");
+}
+else
+{
+    Console.WriteLine("Valor inválido. Por favor, digite um número.");
+}
