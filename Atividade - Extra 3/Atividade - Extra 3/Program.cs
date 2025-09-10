89bc147b77403c4297b30f015125b43bc0e82d46
Maria Eduarda Girondolli <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-10 10:24:27 -0300

    Feat: Atividade - Extra 3

---
+++ Atividade - Extra 3/Atividade - Extra 3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o valor de A: ");
+string inputA = Console.ReadLine();
+
+Console.Write("Digite o valor de B: ");
+string inputB = Console.ReadLine();
+
+Console.Write("Digite o valor de C: ");
+string inputC = Console.ReadLine();
+if (double.TryParse(inputA, out double a) &&
+               double.TryParse(inputB, out double b) &&
+               double.TryParse(inputC, out double c))
+{
+    double delta = b * b - 4 * a * c;
+    Console.WriteLine($"O valor de Δ (delta) é: {delta:F2}");
+}
+else
+{
+    Console.WriteLine("Valores inválidos. Por favor, digite números válidos.");
+}
