bdb938d1e492235c8fed06c7fc8f8f3a0963b2b0
Maria Eduarda <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-16 08:18:32 -0300

    Feat: Atividade - 14(for-while)

---
+++ Atividade - 14 (for-while)/Atividade - 14 (for-while)/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite um valor N (maior que 0): ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= N)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
