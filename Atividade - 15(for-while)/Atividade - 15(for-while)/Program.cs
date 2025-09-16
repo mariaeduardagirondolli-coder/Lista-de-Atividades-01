49559b9d928e6fcd07c2587b412a22581555bb64
Maria Eduarda <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-16 08:25:11 -0300

    Feat: Atividade - 15(for-while)

---
+++ Atividade - 15(for-while)/Atividade - 15(for-while)/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Números entre 1000 e 2000 que deixam resto 5 quando divididos por 11:");
+
+int i = 1000;
+while (i <= 2000)
+{
+    if (i % 11 == 5)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
