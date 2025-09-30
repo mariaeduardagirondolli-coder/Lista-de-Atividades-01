86363f2af5935139ce3e37d80a2152cd041ffd83
Maria Eduarda <maria.eduarda.girondolli.sesi@gmail.com>
2025-09-30 07:51:54 -0300

    Feat: exemplo vetores

---
+++ int [ ] = valores = new int [ ]/int [ ] = valores = new int [ ]/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}° numero : ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+for (int j = 0; j < 5; j++)
+{
+    Console.WriteLine($"O valor da posição [{j}] = {numeros[j]}");
+}
+  
